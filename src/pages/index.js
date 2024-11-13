a8e1932adddff45ad42aee611620192a81a07dc8
RMxNinja <91172425+RupeshMangalam21@users.noreply.github.com>
2024-11-13 15:04:02 +0530

    adding framer motion

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Scene from '../components/Scene';
 export default function Home() {
     return (
         <div>
-            <h1>Welcome to GearBox</h1>
+            <h1>GearBox</h1>
             <Scene />
         </div>
     );
